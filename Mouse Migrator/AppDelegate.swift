73e95edacf372de1be869c291b161e22798185f9
Jimmy Tran <jimmyytran@gmail.com>
2017-06-12 20:58:33 -0700

    Initial Commit

---
+++ Mouse Migrator/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Mouse Migrator
+//
+//  Created by Jimmy Tran on 6/12/17.
+//  Copyright © 2017 Jimmy Tran. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Mouse Migrator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Mouse Migrator
+//
+//  Created by Jimmy Tran on 6/12/17.
+//  Copyright © 2017 Jimmy Tran. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
